b6230f5346deedf5440ccdbe74ac70ad1abe749d
tobiichi3227 <cz1346219@gmail.com>
2026-08-11 22:24:01 +0800

    pD: optimize blame_n78 solution

--- pD/solution/blame_n78.cpp
+++ pD/solution/blame_n78.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define int ll
 const int mod = 1145141;
 int fpw(int a, int b) {
     if(b == 0) return 1;
     int re = fpw(a, b>>1);
-    if(b&1) return re*re%mod*a%mod;
-    return re*re%mod;
+    if(b&1) return 1ll*re*re%mod*a%mod;
+    return 1ll*re*re%mod;
 }
 int pi(int n) {
     int sn = sqrt(n);
-    vector<int> isprime(sn+1, 1), primes;
+    vector<bool> isprime(sn+1, 1);
+    vector<int> primes;
     for(int i = 2; i <= sn; i++) if(isprime[i]) {
         primes.push_back(i);
-        for(int j = i*i; j <= sn; j += i) isprime[j] = 0;
+        for(ll j = 1ll*i*i; j <= sn; j += i) isprime[j] = 0;
     }
     vector<pair<int, int> > v;
     vector<int> mp(sn+5), mp2(sn+5);
@@ -29,37 +29,31 @@ int pi(int n) {
     }
     for(int i = 0; i < primes.size(); i++) {
         auto &p = primes[i];
-        for(int j = 0; j < N && p * p <= v[j].first; j++) {
+        for(int j = 0; j < N && 1ll*p * p <= v[j].first; j++) {
             int fr = v[j].first/p;
             int id = fr <= sn ? mp[fr] : mp2[n/fr];
             v[j].second -= (v[id].second - (i));
         }
     }
-    return N == 0 ? n-1 : v[0].second;
+    return v[0].second;
 }
 signed main() {
-    // (n^(7/8))/log2(n)
     ios::sync_with_stdio(false), cin.tie(0);
     int n; cin >> n;
     int sn = sqrt(n);
-    //int sn = n;
-    vector<int> isprime(sn+1, 1);
+    vector<bool> isprime(sn+1, 1);
     int ans = 1;
     for(int i = 2; i <= sn; i++) if(isprime[i]) {
-        for(int j = i+i; j <= sn; j+=i) isprime[j] = 0;
+        for(ll j = 1ll*i*i; j <= sn; j+=i) isprime[j] = 0;
         int now = 0;
-        for(int j = i; j <= n; j *= i)
+        for(ll j = i; j <= n; j *= i)
             now += n/j;
-        ans = ans * (now + 1)%mod;
+        ans = 1ll*ans * (now + 1)%mod;
     }
-    //cout << ans  << '\n';
-
     for(int l = sn+1, r; l <= n; l = r+1) {
         r = (n/(n/l));
         int k = n/l;
-        //cout << "! " << l << ' ' << r << ' ' << k << '\n';
-
-        ans = ans * fpw(k+1, pi(r) - pi(l-1))%mod;
+        ans = 1ll*ans * fpw(k+1, pi(r) - pi(l-1))%mod;
     }
     cout << ans << '\n';
 }
