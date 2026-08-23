6d77896f50d6b19a0145252d2516912a5536bc14
tobiichi3227 <cz1346219@gmail.com>
2026-08-23 09:57:40 +0800

    pC: add must wrong solution

---
+++ pC/solution/-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n, m, q;
+    std::cin >> n >> m >> q;
+    while (q--) {
+        std::cout << -1 << std::endl;
+    }
+}
